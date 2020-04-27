2400b1f2a0fa7e6a821df6740fa66ae20050762e
root <kevin.sandermann@gmail.com>
2020-04-27 09:17:04 +0000

    update

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "dependency_subscription_id" {
+  value = data.azurerm_subscription.this.id
+}
+
+output "dependency_organisation" {
+  value = var.organisation
+}
+
+output "dependency_environment" {
+  value = var.environment
+}
+
+output "dependency_ocation_main" {
+  value = azurerm_resource_group.main.location
+}
